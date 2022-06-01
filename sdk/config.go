d5f4cb2f1b7598d823c2dbb40d000b6a89955fd9
Daan Gerits <daan.gerits@gmail.com>
2022-06-01 19:52:31 +0200

    WIP: support for configuration

---
+++ sdk/config.go
@@ -0,0 +1,9 @@
+package sdk
+
+var (
+	PixelLength = []byte{0, 4}
+
+	ConfigParams = map[string][]byte{
+		"pixel_length": PixelLength,
+	}
+)
--- sdk/device.go
+++ sdk/device.go
@@ -1,6 +1,10 @@
 package sdk
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"github.com/area3001/goira/comm"
 	"github.com/area3001/goira/core"
@@ -28,34 +32,74 @@ type Hardware struct {
 	Version string `json:"revision"`
 }
 
-func (d *Device) SetMode(mac string, mode *core.Mode) error {
+func (d *Device) SetMode(mode *core.Mode) error {
 	if mode == nil {
 		return fmt.Errorf("no mode provided")
 	}
 
-	return d.nc.Call("mode", mac, []byte(fmt.Sprintf("%d", mode.Code)))
+	return d.nc.Call("mode", d.Meta.MAC, []byte(fmt.Sprintf("%d", mode.Code)))
+}
+
+func (d *Device) SetConfig(param string, value string) error {
+	buf := new(bytes.Buffer)
+
+	b, fnd := ConfigParams[param]
+	if !fnd {
+		return fmt.Errorf("%s: invalid parameter", param)
+	}
+	buf.Write(b)
+	buf.WriteString(value)
+
+	return d.nc.Call("config", d.Meta.MAC, []byte(base64.StdEncoding.EncodeToString(buf.Bytes())))
 }
 
 func (d *Device) Blink(times int) error {
 	return d.nc.Call("blink", d.Meta.MAC, []byte(fmt.Sprintf("%d", times)))
 }
 
-func (d *Device) Reset(mac string, delayMs int) error {
-	return d.nc.Call("reset", mac, []byte(fmt.Sprintf("%d", delayMs)))
+func (d *Device) Reset(delayMs int) error {
+	return d.nc.Call("reset", d.Meta.MAC, []byte(fmt.Sprintf("%d", delayMs)))
 }
 
-func (d *Device) SendDmx(mac string, data []byte) error {
-	return d.nc.Call("dmx", mac, data)
+func (d *Device) SendDmx(data []byte) error {
+	return d.nc.Call("dmx", d.Meta.MAC, data)
 }
 
-func (d *Device) SendDeltaDmx(mac string, data []byte) error {
-	return d.nc.Call("deltadmx", mac, data)
+func (d *Device) SendDeltaDmx(data []byte) error {
+	return d.nc.Call("deltadmx", d.Meta.MAC, data)
 }
 
-func (d *Device) SendRgb(mac string, data []byte) error {
-	return d.nc.Call("rgb", mac, data)
+func (d *Device) SendRgbRaw(data []byte) error {
+	return d.nc.Call("rgb", d.Meta.MAC, []byte(base64.StdEncoding.EncodeToString(data)))
 }
 
-func (d *Device) SendFx(mac string, effect *core.Effect) error {
-	return d.nc.Call("fx", mac, effect.ToBytes())
+func (d *Device) SendRgbPixels(offset int, hexCodes []string) error {
+	// -- construct the packet
+	bytesPerPixel := len(hexCodes[0])
+	if bytesPerPixel != 6 && bytesPerPixel != 8 {
+		return fmt.Errorf("wrong number of pixel channels")
+	}
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, uint16(offset))
+	binary.Write(buf, binary.LittleEndian, uint16(len(hexCodes)))
+
+	for idx, arg := range hexCodes {
+		b, err := hex.DecodeString(arg)
+		if err != nil {
+			return fmt.Errorf("data %s for pixel %d is invalid: %s", arg, idx, err)
+		}
+
+		if len(b) != bytesPerPixel {
+			return fmt.Errorf("data %s for pixel %d is invalid: inconsistent number of bytes per pixel", arg, idx)
+		}
+
+		buf.Write(b)
+	}
+
+	return d.nc.Call("rgb", d.Meta.MAC, []byte(base64.StdEncoding.EncodeToString(buf.Bytes())))
+}
+
+func (d *Device) SendFx(effect *core.Effect) error {
+	return d.nc.Call("fx", d.Meta.MAC, effect.ToBytes())
 }
